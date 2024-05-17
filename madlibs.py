00589a371baa32fb2ae2cafe095de17e897687d6
Muhammed Jaseel K <109283700+mjk859@users.noreply.github.com>
2024-05-17 13:41:05 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,6 @@
+adjective = input("Adjective:")
+verb1 = input("Verb:")
+verb2 = input("Verb:")
+famous_person = input("Famous person:")
+
+print(f"Computer programming is so {adjective}! It makes me so excited all the time because I love to {verb1}. Stay hidrated and {verb2} like {famous_person}.") 
